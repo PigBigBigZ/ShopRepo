c5296880c1757583447a2dc068eee09088f34103
feng <1063009534@qq.com>
2018-09-11 16:42:26 +0800

    1

--- shopping/src/main/java/com/yc/biz/UserBiz.java
+++
@@ -1,9 +0,0 @@
-package com.yc.biz;
-
-public class UserBiz {
-   public static void main(String[] args) {
-	System.out.println("hello");
-	System.out.println("hello");
-	System.out.println("hello");
-}
-}
